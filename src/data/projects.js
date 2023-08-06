cf6deaf15a80e6db1a404b8b999f8cc3a5000e7c
amilpa <amilpa2020@gmail.com>
2023-08-06 13:46:40 +0530

    setup 1

---
+++ src/data/projects.js
@@ -0,0 +1,24 @@
+import ChatAppImage from '../assets/chatapp.png'
+
+export const Projects = [
+  {
+    name: "React chat app",
+    info: "Simple chat app with a single room.Users can join the room and chat with each other.",
+    img: ChatAppImage,
+  },
+  {
+    name: "React chat app",
+    info: "Simple chat app with a single room.Users can join the room and chat with each other.",
+    img: ChatAppImage,
+  },
+  {
+    name: "React chat app",
+    info: "Simple chat app with a single room.Users can join the room and chat with each other.",
+    img: ChatAppImage,
+  },
+  {
+    name: "React chat app",
+    info: "Simple chat app with a single room.Users can join the room and chat with each other.",
+    img: ChatAppImage,
+  },
+]
