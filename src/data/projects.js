c6e50953e514310a908b4ad4e0265397934929f2
amilpa <amilpa2020@gmail.com>
2023-08-12 22:17:08 +0530

    stylings

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,28 +1,19 @@
 import ChatAppImage from "../assets/chatapp.png";
+import NotesAppImage from "../assets/notesapp.jpeg";
 
 export const Project_data = [
   {
     name: "React chat app",
     info: "Simple chat app with a single room.Users can join the room and chat with each other.",
-    stack: "React, Node, Express, Socket.io",
+    stack: "React , Node, Express, Socket.io",
+    link: "https://react-chat-app-amilpa.vercel.app/",
     image: ChatAppImage,
   },
   {
-    name: "React chat app",
-    info: "Simple chat app with a single room.Users can join the room and chat with each other.",
-    stack: "React, Node, Express, Socket.io",
-    image: ChatAppImage,
-  },
-  {
-    name: "React chat app",
-    info: "Simple chat app with a single room.Users can join the room and chat with each other.",
-    stack: "React, Node, Express, Socket.io",
-    image: ChatAppImage,
-  },
-  {
-    name: "React chat app",
-    info: "Simple chat app with a single room.Users can join the room and chat with each other.",
-    stack: "React, Node, Express, Socket.io",
-    image: ChatAppImage,
+    name: "React notes app",
+    info: "Simple notes app where you can save notes to local storage with search feature.",
+    stack: "Reactjs",
+    link: "https://react-notes-app-tau.vercel.app",
+    image: NotesAppImage,
   },
 ]
