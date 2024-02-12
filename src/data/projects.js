61477807edee83f906f534bc6af42965d09a30b7
Amil P A <amilpa2020@gmail.com>
2024-02-12 09:16:26 +0530

    add projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,8 +1,39 @@
+import SageImage from "../assets/Sage.png";
 import ChatAppImage from "../assets/chatapp.png";
-import NotesAppImage from "../assets/notesapp.jpeg";
+import Connect4Image from "../assets/connect-4.jpg";
 import EcommerceImage from "../assets/ecommerce.jpeg";
+import NotesAppImage from "../assets/notesapp.jpeg";
+import Visita from "../assets/visita.jpg";
 
 export const Project_data = [
+  {
+    name: "Visita",
+    info: "This app is designed to help travelers explore new destinations for a more personalized travel experience.",
+    stack: "Flutter, Express.js, Firebase, Cloudinary",
+    link: "https://www.youtube.com/watch?v=Kc6Ow0M1vlk",
+    image: Visita,
+  },
+  {
+    name: "Connect-4",
+    info: "A web-based implementation of the classic Connect 4 board game created using React and Socket.IO.",
+    stack: "React.js , Socket.io",
+    link: "https://www.youtube.com/watch?v=sB6N7P7e4XY",
+    image: Connect4Image,
+  },
+  {
+    name: "Sage",
+    info: "A web application designed specifically for corporate professionals to help them prioritize their mental well-being",
+    stack: "Next.js , Tailwindcss, Firebase, OpenAI",
+    link: "https://www.youtube.com/watch?v=mLvfAb-s0-w",
+    image: SageImage,
+  },
+  {
+    name: "Full stack ecommerce app",
+    info: "Ecommerce app where everyone can sell their products and buy products from other sellers.",
+    stack: "Nextjs ( app router ) ",
+    link: "https://sellfast.vercel.app",
+    image: EcommerceImage,
+  },
   {
     name: "React chat app",
     info: "Simple chat app with a single room.Users can join the room and chat with each other.",
@@ -16,12 +47,5 @@ export const Project_data = [
     stack: "Reactjs",
     link: "https://react-notes-app-tau.vercel.app",
     image: NotesAppImage,
-  },
-  {
-    name: "Full stack ecommerce app",
-    info: "Ecommerce app where everyone can sell their products and buy products from other sellers.",
-    stack: "Nextjs ( app router ) ",
-    link: "https://sellfast.vercel.app",
-    image: EcommerceImage,
   },
 ];
