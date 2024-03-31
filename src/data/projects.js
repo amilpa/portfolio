31525b32e90afb26aa91f83a22239e0c5c185acf
amilpa <amilpa2020@gmail.com>
2024-03-31 23:46:49 +0530

    add data

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,47 +4,55 @@ import Connect4Image from "../assets/connect-4.jpg";
 import EcommerceImage from "../assets/ecommerce.jpeg";
 import NotesAppImage from "../assets/notesapp.jpeg";
 import Visita from "../assets/visita.jpg";
+import Orma from "../assets/Orma.png";
+import Fasal from "../assets/Fasal.png";
 
 export const Project_data = [
   {
+    name: "Orma",
+    info: "Orma, a memory journal app tailored for individuals in the early stages of dementia.",
+    link: "https://www.youtube.com/watch?v=YiZTMH_aplY&t=76s",
+    image: Orma,
+  },
+  {
+    name: "Fasal",
+    info: "An agricultural application which revolutionizes farming with data-driven predictions.",
+    link: "https://youtu.be/QGSFvOm3z98?si=KrywB47n85NLQBLB",
+    image: Fasal,
+  },
+  {
+    name: "Sage",
+    info: "Web app prioritizing mental well-being for corporate professionals' needs.",
+    link: "https://www.youtube.com/watch?v=mLvfAb-s0-w",
+    image: SageImage,
+  },
+  {
     name: "Visita",
-    info: "This app is designed to help travelers explore new destinations for a more personalized travel experience.",
-    stack: "Flutter, Express.js, Firebase, Cloudinary",
+    info: "Travel app tailors destinations for personalized exploration experiences.",
     link: "https://www.youtube.com/watch?v=Kc6Ow0M1vlk",
     image: Visita,
   },
   {
     name: "Connect-4",
-    info: "A web-based implementation of the classic Connect 4 board game created using React and Socket.IO.",
-    stack: "React.js , Socket.io",
+    info: "Connect 4 web game built with React and Socket.IO for multiplayer functionality.",
     link: "https://www.youtube.com/watch?v=sB6N7P7e4XY",
     image: Connect4Image,
   },
   {
-    name: "Sage",
-    info: "A web application designed specifically for corporate professionals to help them prioritize their mental well-being",
-    stack: "Next.js , Tailwindcss, Firebase, OpenAI",
-    link: "https://www.youtube.com/watch?v=mLvfAb-s0-w",
-    image: SageImage,
-  },
-  {
     name: "Full stack ecommerce app",
-    info: "Ecommerce app where everyone can sell their products and buy products from other sellers.",
-    stack: "Nextjs ( app router ) ",
+    info: "Universal ecommerce platform for buying and selling various products.",
     link: "https://sellfast.vercel.app",
     image: EcommerceImage,
   },
   {
     name: "React chat app",
-    info: "Simple chat app with a single room.Users can join the room and chat with each other.",
-    stack: "React , Node, Express, Socket.io",
+    info: "Basic chat app with one room for users to join and chat.",
     link: "https://react-chat-app-amilpa.vercel.app/",
     image: ChatAppImage,
   },
   {
     name: "React notes app",
-    info: "Simple notes app where you can save notes to local storage with search feature.",
-    stack: "Reactjs",
+    info: "Basic notes app with local storage and search function.",
     link: "https://react-notes-app-tau.vercel.app",
     image: NotesAppImage,
   },
